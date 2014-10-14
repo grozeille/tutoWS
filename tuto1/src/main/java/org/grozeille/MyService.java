d5104eec0d6dcac45a4d7c0c4d316364da87378e
Mathias Kluba <mathias.kluba@gmail.com>
2014-10-14 08:38:31 +0200

    migration to new cxf + add swagger

--- tuto1/src/main/java/org/grozeille/MyService.java
+++ tuto1/src/main/java/org/grozeille/MyService.java
@@ -1,5 +1,7 @@
 package org.grozeille;
 
+import com.wordnik.swagger.annotations.*;
+
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
@@ -7,23 +9,25 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
 @WebService
-@Path("/myservice")
-@Produces("application/json")
-//@Produces({ "text/xml" })
-//@Produces({ "text/plain" })
+@Path("/")
+@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+@Api(value = "/", description = "Calculator")
 public interface MyService {
 	
 	@WebMethod
 	@GET
 	@Path("/plus")
-//	@Produces("application/json")
-	Result plus(
+    @ApiOperation(value = "Addition", notes = "More notes about this method", response = Result.class)
+	public Result plus(
 			@QueryParam("a")
-			@WebParam(name ="a") 
+			@WebParam(name ="a")
+            @ApiParam(value = "first value", required = true)
 			int a, 
 			@QueryParam("b")
 			@WebParam(name ="b")
+            @ApiParam(value = "second value", required = true)
 			int b);
 }
--- tuto1/src/main/java/org/grozeille/Result.java
+++ tuto1/src/main/java/org/grozeille/Result.java
@@ -1,11 +1,18 @@
 package org.grozeille;
 
+import com.wordnik.swagger.annotations.ApiModel;
+import com.wordnik.swagger.annotations.ApiModelProperty;
+
 import javax.xml.bind.annotation.XmlRootElement;
 
+@ApiModel(value = "The result of an operation")
 @XmlRootElement()
 public class Result {
-	private int value;
-	
+
+    @ApiModelProperty(value = "result of the operation")
+    private int value;
+
+    @ApiModelProperty(value = "the operation")
 	private String message;
 
 	public String getMessage() {

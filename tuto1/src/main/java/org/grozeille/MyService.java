ea7c3371a42d9215e9b60148a5d66c13c27a47ce
Mathias Kluba <mathias.kluba@gmail.com>
2014-10-15 00:21:23 +0200

    oops

--- tuto1/src/main/java/org/grozeille/MyService.java
+++ tuto1/src/main/java/org/grozeille/MyService.java
@@ -12,9 +12,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @WebService
-@Path("/")
 @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-@Api(value = "/", description = "Calculator")
 @Path("/myservice")
 @Api(value = "/myservice", description = "Calculator")
 public interface MyService {
